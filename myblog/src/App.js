a4e8c6b85a53c6169dc5a600fdc1619e12eebee8
lovegnep <lovegnep@163.com>
2018-04-09 20:54:29 +0800

    修复富文本编辑器光标错位BUG

--- myblog/src/App.js
+++ myblog/src/App.js
@@ -5,6 +5,7 @@ import home from "./pages/home/home";
 import login from './pages/login/login';
 import theme from './pages/blog/theme';
 import NewTheme from './pages/blog/new';
+import Tmpnew from './pages/blog/tmpnew';
 import EditTheme from './pages/blog/edit';
 class App extends Component {
 render(){
@@ -14,7 +15,8 @@ render(){
           <Route path="/" exact component={home} />
           <Route path="/login" component={login} />
           <Route path="/theme/:id" component={theme} />
-          <Route path="/newtheme" component={NewTheme} />
+          <Route path="/newtheme" component={Tmpnew} />
+          <Route path="/tmpnew" component={NewTheme} />
           <Route path="/edittheme" component={EditTheme} />
           <Redirect to="/" />
         </Switch>
---
+++ myblog/src/pages/blog/tmpnew.jsx
@@ -0,0 +1,132 @@
+import React, {Component} from 'react';
+import {is, fromJS} from 'immutable';
+import API from '../../api/api';
+import {Head} from '../components/head';
+import PublicHeader from '../components/header/header';
+import Addtype from './addtype';
+import './tmpnew.scss';
+import Editor from '../editor/editor';
+class Tmpnew extends Component {
+    constructor(props) {
+        super(props);
+        let state = this.props.location.state;
+        let tmp = false;
+        if (state && state.loginStatus) {
+            tmp = true;
+        } else {
+            tmp = false;
+        }
+        this.loginStatus = tmp;
+        this.state = {
+            typeList: [],
+            isplay: false
+        };
+        this.title = '';
+        this.tab = '';
+    }
+
+    getTypes = async () => {
+        let result = await API.getTypeList();
+        this.setState({typeList: result.data});
+    }
+
+    componentWillMount() {
+        //this.getTypes();
+    }
+
+    componentDidMount() {
+        this.getTypes();
+    }
+
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state), fromJS(nextState))
+    }
+
+    handleTabChange(e) {
+        let select = e.target;
+        if (select.value == '新建分类') {
+            this.setState({isplay: true});
+        } else {
+            this.tab = select.value;
+        }
+
+    }
+
+    addType = async (obj) => {
+        let result = await API.addType(obj);
+        this.setState({isplay: false});
+        this.getTypes();
+    }
+
+    onAddType(obj) {
+        switch (obj.event) {
+            case 1:
+                this.setState({isplay: false});
+                break;
+            case 2:
+                this.addType({tab: obj.typename});
+                break;
+            default:
+                break;
+        }
+    }
+
+    onTitleChange(e) {
+        this.title = e.target.value;
+    }
+
+    addNewTheme = async (obj) => {
+        let result = await API.addNewTheme(obj);
+        this.props.history.push({
+            pathname: '/theme/' + result._id,
+            state: {
+                loginStatus: true
+            },
+        });
+    }
+
+    onSubmit(value) {
+        if (this.title == '' || this.title.length < 5) {
+            return alert('标题不能少于5个字符');
+        }
+        if (this.tab == '' || this.tab.length < 2) {
+            return alert('分类名称不能少于2个字符');
+        }
+        this.addNewTheme({tab: this.tab, title: this.title, t_content: value});
+    }
+
+    render() {
+        let self = this;
+        return (
+            <div className="newcontain">
+                <PublicHeader title='新建文章' loginStatus={true}/>
+                <ul className="formtt">
+                    <li><span>标题</span>
+                        <input onChange={self.onTitleChange.bind(self)}/>
+                    </li>
+                    <li><span>分类</span>
+                        <select name="tab" onChange={self.handleTabChange.bind(self)}>
+                            <option selected disabled>--请选择--</option>
+                            <option>新建分类</option>
+                            {
+                                this.state.typeList.map(function (type, index) {
+                                    return <option key={index}>{type}</option>
+                                })
+                            }
+                        </select>
+                    </li>
+
+                </ul>
+                <div className="edit_draft">
+                    <Editor cb={self.onSubmit.bind(self)}/>
+                </div>
+                <Addtype isplay={this.state.isplay} cb={this.onAddType.bind(self)}/>
+            </div>
+        )
+
+    }
+
+}
+
+export default Tmpnew;
