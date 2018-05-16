5f9d6a0f9399a70ee9fc47e4bec79c5cee44b7c1
lovegnep <lovegnep@163.com>
2018-05-16 16:28:15 +0800

    dd

--- server/canvas-img/validate.js
+++ server/canvas-img/validate.js
@@ -3,7 +3,8 @@
  */
 
 var MdCode = module.exports;
-const { createCanvas, loadImage } = require('canvas')
+var Canvas = require('canvas');
+
 
 function randomNum(min,max){
     return Math.floor(Math.random()*(max-min)+min);
@@ -23,7 +24,7 @@ var getRandom = function(start,end){
 MdCode.create = function() {
     var width = 120;
     var height = 35;
-    var canvas = createCanvas(width, height);
+    var canvas = new Canvas(width, height);
     var ctx = canvas.getContext('2d');
     // ctx.textBaseline = 'bottom';
     //** 绘制背景色 **//
